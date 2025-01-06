ecb555dc8758fa9c5543f4ad9b1276cb7ac080bc
rikiTada <riki.tada777@gmail.com>
2025-01-06 14:23:29 +0900

    🩹  useSearchParamsを使用してパラメータを取得するように変更

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import Link from "next/link";
+import { useParams, useSearchParams } from "next/navigation";
+
 type TestPageProps = {
   title: string;
   text: string;
@@ -12,19 +16,16 @@ type PageProps = {
   searchParams: SearchParams;
 };
 
-export default async function TestPage(props: PageProps) {
-  const { params, searchParams } = props;
+export default function TestPage(props: PageProps) {
+  const searchParams = Object.fromEntries(useSearchParams().entries());
 
   return (
     <div className="min-h-dvh m-2">
+      <header>
+        <Link href={"/"}>HOME</Link>
+      </header>
       <h1>Test ページ</h1>
 
-      <div className="m-2 rounded-md bg-gray-100 p-4">
-        params
-        <pre>
-          <code>{JSON.stringify(params, null, 2)}</code>
-        </pre>
-      </div>
       <div className="m-2 rounded-md bg-gray-100 p-4">
         searchParams
         <pre>
