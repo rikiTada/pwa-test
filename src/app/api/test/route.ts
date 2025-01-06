4dfe1ff53a001f6fa9f53c4f40088d5b266255c0
rikiTada <riki.tada777@gmail.com>
2025-01-06 13:21:19 +0900

    ✨GETリクエストのレスポンスにタイトル、テキスト、URLを追加

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,8 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_req: NextRequest) {
+type Params = Promise<{ title: string; text: string; url: string }>;
+
+export async function GET(_req: Request, segmentData: { params: Params }) {
+  const params = await segmentData.params;
+  const { title, text, url } = params;
+
   try {
-    return NextResponse.json({ message: "hello,world" }, { status: 200 });
+    return NextResponse.json(
+      {
+        title,
+        text,
+        url,
+        message: "Hello, World!!",
+      },
+      { status: 200 }
+    );
   } catch (error: unknown) {
     return NextResponse.json({ message: error }, { status: 400 });
   }
