4d4416314e8bd3272cfdc527411bf8ed322f6d9a
rikiTada <riki.tada777@gmail.com>
2025-01-06 13:27:57 +0900

    ✨TestPageコンポーネントにsearchParamsを追加し、paramsとsearchParamsの内容を表示

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,21 +4,32 @@ type TestPageProps = {
   url: string;
 };
 
+type Params = Promise<Partial<TestPageProps>>;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
 type PageProps = {
-  params: Promise<Partial<TestPageProps>>;
+  params: Params;
+  searchParams: SearchParams;
 };
 
-export default async function TestPage({ params }: PageProps) {
-  const { title, text, url } = await params;
+export default async function TestPage(props: PageProps) {
+  const { params, searchParams } = props;
 
   return (
     <div className="min-h-dvh m-2">
       <h1>Test ページ</h1>
 
       <div className="m-2 rounded-md bg-gray-100 p-4">
-        <h1>{title}</h1>
-        <p>{text}</p>
-        <a href={url}>Link</a>
+        params
+        <pre>
+          <code>{JSON.stringify(params, null, 2)}</code>
+        </pre>
+      </div>
+      <div className="m-2 rounded-md bg-gray-100 p-4">
+        searchParams
+        <pre>
+          <code>{JSON.stringify(searchParams, null, 2)}</code>
+        </pre>
       </div>
     </div>
   );
