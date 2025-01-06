5302d3de90130b8eb2e2ff15e2261262000d506d
rikiTada <riki.tada777@gmail.com>
2025-01-06 14:38:07 +0900

    buildエラーを解消

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 type TestPageProps = {
   title: string;
@@ -17,8 +18,6 @@ type PageProps = {
 };
 
 export default function TestPage(props: PageProps) {
-  const searchParams = Object.fromEntries(useSearchParams().entries());
-
   return (
     <div className="min-h-dvh m-2">
       <header>
@@ -26,12 +25,22 @@ export default function TestPage(props: PageProps) {
       </header>
       <h1>Test ページ</h1>
 
-      <div className="m-2 rounded-md bg-gray-100 p-4">
-        searchParams
-        <pre>
-          <code>{JSON.stringify(searchParams, null, 2)}</code>
-        </pre>
-      </div>
+      <Suspense fallback={<div>loading...</div>}>
+        <CodeComp />
+      </Suspense>
     </div>
   );
 }
+
+const CodeComp = () => {
+  const searchParams = Object.fromEntries(useSearchParams().entries());
+
+  return (
+    <div className="m-2 rounded-md bg-gray-100 p-4">
+      searchParams
+      <pre>
+        <code>{JSON.stringify(searchParams, null, 2)}</code>
+      </pre>
+    </div>
+  );
+};
